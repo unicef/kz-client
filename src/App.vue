0d2df96681a8ac4639582cbe813e280393013f1b
dneprm <mariya.turko@iskyweb.com>
2019-06-24 12:22:11 +0300

    feat(user): add log out user on 15 minutes of inactivity

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,55 @@
   
   export default {
       name: 'app',
+      data() {
+        return {
+          idleTimer: null,
+        };
+      },
       created() {
         this.$store.dispatch('global/fetchLanguagesData');
         if (store.getters['auth/auth/isAuthenticated']) {
           this.$store.dispatch('auth/auth/getMyInfo');
         }
+
+        // Add user idle time detection
+        this.refreshIdleTimer();
+        document.addEventListener('mousedown', this.refreshIdleTimer);
+        document.addEventListener('mousemove', this.refreshIdleTimer);
+        document.addEventListener('keydown', this.refreshIdleTimer);
+        document.addEventListener('touchstart', this.refreshIdleTimer);
+      },
+      destroyed() {
+        clearTimeout(this.idleTimer);
+        document.removeEventListener('mousedown', this.refreshIdleTimer);
+        document.removeEventListener('mousemove', this.refreshIdleTimer);
+        document.removeEventListener('keydown', this.refreshIdleTimer);
+        document.removeEventListener('touchstart', this.refreshIdleTimer);
+      },
+      computed: {
+        isAuthenticated() {
+          return store.getters['auth/auth/isAuthenticated'];
+        },
+        isAdminPath() {
+          return this.$route.path.indexOf('admin') !== -1;
+        },
+      },
+      methods: {
+        refreshIdleTimer() {
+          if (!this.isAuthenticated) {
+            return;
+          }
+          clearTimeout(this.idleTimer);
+          this.idleTimer = setTimeout(() => {
+            // Log out the user on 15 minutes of inactivity
+            this.$store.dispatch('auth/auth/logOut');
+            if (this.isAdminPath) {
+              this.$router.push('/auth/login-admin');
+            } else {
+              this.$router.push('/auth/login');
+            }
+          }, 15 * 60 * 1000);
+        },
       },
   };
 </script>
