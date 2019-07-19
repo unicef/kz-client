fa7048a753f97050399b0d385b13dc8347b67cbe
dneprm <mariya.turko@iskyweb.com>
2019-07-19 14:55:29 +0300

    style(auth): change code style at login form

--- src/modules/Auth/components/LoginForm.vue
+++ src/modules/Auth/components/LoginForm.vue
@@ -1,83 +1,94 @@
 <template>
-    <v-form ref="form" @submit.prevent="submit" align-center justify-center lazy-validation class="mt-4 pb-4">
-      <v-card class="login-form">
-        <v-toolbar class="headline justify-center" color="light-blue">{{ $t('signIn.title') }}</v-toolbar>
-        <v-container :class='{ "pt-4": $vuetify.breakpoint.xs }'>
-          <v-card-text>
-            <v-container grid-list-md class="px-0 pb-0" :class='{ "px-0": $vuetify.breakpoint.xs }'>
-              <v-layout row>
-                <v-flex sm12>
-                  <v-text-field
-                    name="Email"
-                    :label='$t("common.fields.login") + "*"'
-                    id="login"
-                    v-model="credentials.email"
-                    type="email"
-                    :rules="rules.login"
-                    required
-                  />
-                </v-flex>
-              </v-layout>
-              <v-layout row>
-                <v-flex sm12>
-                  <v-text-field
-                    name="Password"
-                    :label='$t("common.fields.password") + "*"'
-                    id="password"
-                    v-model="credentials.password"
-                    :append-icon="showPassword ? 'visibility' : 'visibility_off'"
-                    :type="showPassword ? 'text' : 'password'"
-                    :rules="rules.password"
-                    @click:append="showPassword = !showPassword"
-                    required
-                  />
-                </v-flex>
-              </v-layout>
-              <v-layout row>
-                <v-flex sm12 >
-                  <vue-recaptcha
-                    style="transform:scale(0.8);transform-origin:0 0"
-                    class='recaptcha-modal'
-                    ref="recaptcha"
-                    @verify="verify"
-                    sitekey="6Lem0KYUAAAAAPmN2LyGYEhbWeni_CzNjFVDXHG5"></vue-recaptcha>
-                    <captcha-error v-if='recaptchaVerified' />
-                </v-flex>
-              </v-layout>
-              <!-- Alert -->
-              <v-layout row v-if="errorAlert.state">
-                <v-flex sm12>
-                  <v-alert :value="errorAlert.state" type="error">{{ errorAlert.msg }}</v-alert>
-                </v-flex>
-              </v-layout>
-              <!-- Success -->
-              <v-layout row v-if="successAlert.state">
-                <v-flex sm12>
-                  <v-alert :value="successAlert.state" type="success">{{ successAlert.msg }}</v-alert>
-                </v-flex>
-              </v-layout>
-              <v-layout row v-if="showResendLink">
-                <v-flex sm12>
-                  <v-btn color="info mb-2 mt-2" @click="sendActivationLink" depressed>{{ $t('common.btns.sendActivationLink') }}</v-btn>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-layout align-center class="btns-wrapper">
-              <a
-                  v-if="!isLoginAdmin"
-                  class='text-xs-center' 
-                  @click='forgotPassword'>
-                  {{ $t('common.btns.forgotPassword') }}
-                </a>
-              <v-btn type="submit" color="info mb-2 mt-2" depressed>{{ $t('common.btns.signIn') }}</v-btn>
+  <v-form
+    ref="form"
+    @submit.prevent="submit"
+    align-center
+    justify-center
+    lazy-validation
+    class="mt-4 pb-4"
+  >
+    <v-card class="login-form">
+      <v-toolbar class="headline justify-center" color="light-blue">{{ $t('signIn.title') }}</v-toolbar>
+      <v-container :class="{ 'pt-4': $vuetify.breakpoint.xs }">
+        <v-card-text>
+          <v-container grid-list-md class="px-0 pb-0" :class="{ 'px-0': $vuetify.breakpoint.xs }">
+            <v-layout row>
+              <v-flex sm12>
+                <v-text-field
+                  name="Email"
+                  :label='$t("common.fields.login") + "*"'
+                  id="login"
+                  v-model="credentials.email"
+                  type="email"
+                  :rules="rules.login"
+                  required
+                />
+              </v-flex>
             </v-layout>
-          </v-card-actions>
-        </v-container>
-      </v-card>
-    </v-form>
+            <v-layout row>
+              <v-flex sm12>
+                <v-text-field
+                  name="Password"
+                  :label='$t("common.fields.password") + "*"'
+                  id="password"
+                  v-model="credentials.password"
+                  :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+                  :type="showPassword ? 'text' : 'password'"
+                  :rules="rules.password"
+                  @click:append="showPassword = !showPassword"
+                  required
+                />
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex sm12>
+                <vue-recaptcha
+                  style="transform:scale(0.8);transform-origin:0 0"
+                  class="recaptcha-modal"
+                  ref="recaptcha"
+                  @verify="verify"
+                  sitekey="6Lem0KYUAAAAAPmN2LyGYEhbWeni_CzNjFVDXHG5"
+                ></vue-recaptcha>
+                <captcha-error v-if="recaptchaVerified" />
+              </v-flex>
+            </v-layout>
+            <!-- Alert -->
+            <v-layout row v-if="errorAlert.state">
+              <v-flex sm12>
+                <v-alert :value="errorAlert.state" type="error">{{ errorAlert.msg }}</v-alert>
+              </v-flex>
+            </v-layout>
+            <!-- Success -->
+            <v-layout row v-if="successAlert.state">
+              <v-flex sm12>
+                <v-alert :value="successAlert.state" type="success">{{ successAlert.msg }}</v-alert>
+              </v-flex>
+            </v-layout>
+            <v-layout row v-if="showResendLink">
+              <v-flex sm12>
+                <v-btn
+                  color="info mb-2 mt-2"
+                  @click="sendActivationLink"
+                  depressed
+                >{{ $t('common.btns.sendActivationLink') }}</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-layout align-center class="btns-wrapper">
+            <a
+              v-if="!isLoginAdmin"
+              class="text-xs-center"
+              @click="forgotPassword"
+            >{{ $t('common.btns.forgotPassword') }}</a>
+            <v-btn type="submit" color="info mb-2 mt-2" depressed>{{ $t('common.btns.signIn') }}</v-btn>
+          </v-layout>
+        </v-card-actions>
+      </v-container>
+    </v-card>
+  </v-form>
 </template>
 
 <script>
@@ -150,7 +161,7 @@
             this.credentials['g-recaptcha-response'] = '';
             this.errorAlert.state = false;
             this.errorAlert.msg = '';
-            if(this.isLoginAdmin) {
+            if (this.isLoginAdmin) {
               this.$router.push({ name: 'unicef-users-list' });
             } else {
               this.$router.push({ name: 'user-details' });
