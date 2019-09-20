ebee68aef8cc4864d026a07540aff43cb0c968dc
dneprm <mariya.turko@iskyweb.com>
2019-09-20 11:59:41 +0300

    fix(face-request): disable rejection reason field at face-request form if status is reject and isMyStage

--- src/modules/Dashboard/components/projects/faceRequests/FaceRequestDetails.vue
+++ src/modules/Dashboard/components/projects/faceRequests/FaceRequestDetails.vue
@@ -304,7 +304,7 @@
                     :label="$t('common.fields.rejectReason')"
                     v-model="activities[index].rejectReason"
                     :placeholder="$t('common.fields.rejectReason')"
-                    :disabled="!isMyStage"
+                    :disabled="!isMyStage || (isMyStage && activities[index].isRejected)"
                     :ref="'rejectReason' + index"
                     :rules="rules.fieldRequired"
                     type="text"
